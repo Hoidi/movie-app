83bbb1b4b880a812c2ba5d8de755e78ae9af245d
Tobias Karlsson <github@hoidi.se>
2023-03-17 11:03:17 +0100

    Add grid route for displaying few set movies

---
+++ src/routes/grid/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import MovieLayout from '../../movies/MovieLayout.svelte';
+    import { movieStore } from '../../store/movies';
+    import type { PageServerData } from './$types';
+
+    export let data: PageServerData;
+    movieStore.setFromPromiseList(data.myMoviesFetch);
+</script>
+
+<h1>Grid of movies</h1>
+
+<MovieLayout layout="grid"/>
