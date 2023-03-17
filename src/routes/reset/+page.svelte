60931300df1d8bb3421b6be6b90bb872acfc2c4d
Tobias Karlsson <github@hoidi.se>
2023-03-17 15:30:28 +0100

    Move reset logic from +page.ts to +page.svelte

--- src/routes/reset/+page.svelte
+++ src/routes/reset/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+    import { diaryStore } from "../../store/diary";
+    import { movieStore } from "../../store/movies";
+    import { personStore } from "../../store/people";
+
+    movieStore.reset();
+    diaryStore.reset();
+    personStore.reset();
+</script>
+
 <h1>Resetting stores</h1>
