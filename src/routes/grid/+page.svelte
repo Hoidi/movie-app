85bb601f048e16966cc9eff3eb574214848628c9
Tobias Karlsson <github@hoidi.se>
2023-03-31 09:05:02 +0200

    Add yellow alert if diary is empty

--- src/routes/grid/+page.svelte
+++ src/routes/grid/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+    import { Alert } from 'flowbite-svelte';
     import MovieLayout from '../../movies/MovieLayout.svelte';
+    import MovieList from '../../movies/MovieList.svelte';
     import { movieStore } from '../../store/movies';
     import type { PageData } from './$types';
 
     export let data: PageData;
+
+    let movieStorage = $movieStore;
 
     if (data.response) {
         movieStore.upsertFromList(data.response);
@@ -12,4 +16,18 @@
 
 <h1>Grid of movies</h1>
 
-<MovieLayout layout="grid"/>
+{#if Object.keys(movieStorage).length == 0}
+    <div style="margin-top: 50px;">
+        <Alert color="yellow">
+            <span class="font-medium">No diary available. </span> Upload your
+            diary <a href="/upload">here</a> and try again.
+        </Alert>
+    </div>
+    ho
+{:else}
+    <MovieLayout layout="grid" />
+    hi
+    <MovieList />
+{/if}
+
+hello
