91c8e6c5866f08074722d05f95ef323b2afc857c
Tobias Karlsson <github@hoidi.se>
2023-03-31 09:06:24 +0200

    Add MovieList

---
+++ src/movies/MovieList.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import {
+        Rating,
+        Table,
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+    } from 'flowbite-svelte';
+    import { onDestroy } from 'svelte';
+    import { movieStore } from '../store/movies';
+    import type { Movie } from '../types/movie';
+
+    let movies: Movie[] = [];
+
+    const unsub = movieStore.subscribe((storedMovies) => {
+        movies = Object.values(storedMovies);
+    });
+
+    onDestroy(unsub);
+</script>
+
+<Table hoverable>
+    <TableHead>
+        <TableHeadCell>Title</TableHeadCell>
+        <TableHeadCell>Average rating</TableHeadCell>
+        <TableHeadCell>Your rating</TableHeadCell>
+        <TableHeadCell>Genre</TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+        {#each movies as movie}
+            <TableBodyRow>
+                <TableBodyCell>{movie.title}</TableBodyCell>
+                <TableBodyCell>
+                    <Rating
+                        total={5}
+                        rating={Math.round(movie.voteAverage / 2)}
+                    >
+                        <p
+                            slot="text"
+                            class="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400"
+                        >
+                            {(movie.voteAverage / 2).toFixed(1)} out of 5
+                        </p>
+                    </Rating>
+                </TableBodyCell>
+                <TableBodyCell>Unknown rating</TableBodyCell>
+                <TableBodyCell>{movie.genres.join(', ')}</TableBodyCell>
+            </TableBodyRow>
+        {/each}
+    </TableBody>
+</Table>
