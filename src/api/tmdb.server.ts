a7b9a6b1c6ea2cdb9f046d14f26ed0c4677e7d23
Tobias Karlsson <github@hoidi.se>
2023-04-20 08:05:01 +0200

    Add code for adding series to the results later
    Won't do more right now because it would mess
    up the types.

--- src/api/tmdb.server.ts
+++ src/api/tmdb.server.ts
@@ -1,8 +1,12 @@
 import { SECRET_TMDB_ACCESS_TOKEN } from '$env/static/private';
 import TMDB, {
     type Credits,
+    type Movie,
     type MovieDetails,
     type PersonDetail,
+    type Search,
+    type TV,
+    type TvShowDetails,
 } from 'tmdb-ts';
 
 const tmdb = new TMDB(SECRET_TMDB_ACCESS_TOKEN);
@@ -13,6 +17,18 @@ export async function getMovie(id: number): Promise<MovieDetails | undefined> {
 
         if (movie != undefined) {
             return movie;
+        }
+    } catch (err) {
+        return undefined;
+    }
+}
+
+async function getSerie(id: number): Promise<TvShowDetails | undefined> {
+    try {
+        const serie = await tmdb.tvShows.details(id);
+
+        if (serie != undefined) {
+            return serie;
         }
     } catch (err) {
         return undefined;
@@ -53,32 +69,36 @@ export async function searchMovie(
             year: releaseYear,
         });
 
-        const movie = movies.results.find((movie) => {
-            const movieYearString = movie.release_date.slice(0, 4);
-            const movieYearNumber = Number(movieYearString);
-            return (
-                movieYearNumber === releaseYear &&
-                movie.title.toLowerCase() === title.toLowerCase()
-            );
-        });
+        const movie = findMovieWithDiff(movies, title, releaseYear, 0);
 
         if (movie != undefined) {
             return await getMovie(movie.id);
         }
 
         // sometimes the release year differes by one
-        const movieSecond = movies.results.find((movie) => {
-            const movieYearString = movie.release_date.slice(0, 4);
-            const movieYearNumber = Number(movieYearString);
-            return (
-                between(movieYearNumber, releaseYear - 1, releaseYear + 1) &&
-                movie.title.toLowerCase() === title.toLowerCase()
-            );
-        });
+        const movieSecond = findMovieWithDiff(movies, title, releaseYear, 1);
 
         if (movieSecond != undefined) {
             return await getMovie(movieSecond.id);
         }
+
+        /*const series = await tmdb.search.tvShows({
+            query: title,
+            first_air_date_year: releaseYear,
+        });
+
+        const serie = findSerieWithDiff(series, title, releaseYear, 0);
+
+        if (serie != undefined) {
+            return await getSerie(serie.id);
+        }
+
+        // sometimes the release year differes by one
+        const seriesSecond = findSerieWithDiff(series, title, releaseYear, 1);
+
+        if (seriesSecond != undefined) {
+            return await getSerie(seriesSecond.id);
+        }*/
     } catch (err) {
         return undefined;
     }
@@ -89,3 +109,33 @@ function between(value: number, first: number, last: number) {
         upper = Math.max(first, last);
     return value >= lower && value <= upper;
 }
+
+const findMovieWithDiff = (
+    searchResults: Search<Movie>,
+    title: string,
+    releaseYear: number,
+    diff: number
+): Movie | undefined => {
+    return searchResults.results.find((movie) => {
+        const movieYear = Number(movie.release_date.slice(0, 4));
+        return (
+            between(movieYear, releaseYear - diff, releaseYear + diff) &&
+            movie.title.toLowerCase() === title.toLowerCase()
+        );
+    });
+};
+
+const findSerieWithDiff = (
+    searchResults: Search<TV>,
+    title: string,
+    releaseYear: number,
+    diff: number
+): TV | undefined => {
+    return searchResults.results.find((series) => {
+        const seriesYear = Number(series.first_air_date.slice(0, 4));
+        return (
+            between(seriesYear, releaseYear - diff, releaseYear + diff) &&
+            series.name.toLowerCase() === title.toLowerCase()
+        );
+    });
+};
